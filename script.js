93a383739fd43c65d80c3d2f18d9820e4f3ab177
M.Estefane <estefanemsantos@gmail.com>
2026-08-06 21:59:20 -0300

    Enhance verificar function with age category
    Updated the verificar function to include age category and improved result display.

--- script.js
+++ script.js
@@ -9,44 +9,54 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano  -  Number(fano.value) 
         var genero  = ' '
+        var cat  = ''
         var img = document.createElement('img')
         img.setAttribute('id' , 'foto')
         if(fsex[0].checked){
             genero = 'Homem'
             if( idade >= 0 && idade <= 12){
                 //crianca
-                
+                cat  = 'Criança'
                 img.setAttribute('src', 'hcrianca.png')
             }else if( idade < 17){
                 //jovem
+                cat = 'Jovem'
                 img.setAttribute('src', 'hjovem.png')
             }else if( idade < 59){
                 //Adulta
-                
+                cat = 'Adulto'
                 img.setAttribute('src','hadulto.png')
             }else{
                 //Idosa
+                cat = 'Idoso'
                 img.setAttribute('src','hidoso.png')
             }
         }else if (fsex[1].checked){
             genero = 'Mulher'
             if( idade >= 0 && idade <=12){
                 //crianca
+                cat = 'Criança'
                 img.setAttribute('src', 'mcrianca.png')
             }else if( idade < 17){
                 //jovem
+                cat = 'Jovem'
                 img.setAttribute('src', 'mjovem.png')
             }else if( idade < 59){
                 //Adulta
+                cat = 'Adulta'
                 img.setAttribute('src','madulta.png')
             }else{
                 //Idosa
                 img.setAttribute('src','midosa.png')
             }
+
         }
         res.style.textAlign = 'center'
-        res.innerHTML =`<h2>Resultado</h2>Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = '<h2>Resultado </h2>'
         res.appendChild(img)
+        res.innerHTML +=`<p class='txtidade'> ${idade} anos</p>`
+        res.innerHTML += `<p class ='txtcat'> Voce  é  ${cat}</p>`
+       fano.value = ''
     }
 
 
